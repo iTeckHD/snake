5e4d7bd684f58c5cf54f5a08af2a453ca92f3972
Tuan Kha Tran <ituankha.tran+github@gmail.com>
2019-05-24 17:28:11 +0200

    perf(collision): improve collision check

--- src/game/collision_check.ts
+++ src/game/collision_check.ts
@@ -3,13 +3,11 @@ import { GameConfig } from './config';
 
 export const collisionCheck = (coords: Coordination[]) => {
   return coords.some(
-    (c1, c1i) =>
+    c1 =>
       c1.x < 0 ||
       c1.y < 0 ||
-      c1.x >= GameConfig.fieldSize ||
-      c1.y >= GameConfig.fieldSize ||
-      coords
-        .filter((c2, c2i) => c1i !== c2i)
-        .some(c2 => c1.x === c2.x && c1.y === c2.y),
+      c1.x > GameConfig.fieldSize - 1 ||
+      c1.y > GameConfig.fieldSize - 1 ||
+      coords.filter(c2 => c1.x === c2.x && c1.y === c2.y).length > 1,
   );
 };
