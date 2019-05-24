b3c0f981e48bcd79303a6a070d4d614395f9f728
Tuan Kha Tran <tran.tuankha@muffag.ch>
2019-05-24 14:36:16 +0200

    feat(redux): redux store debugging

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,16 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { rootReducers } from './root_reducers';
 
+type MyWindow = Window & {
+  __REDUX_DEVTOOLS_EXTENSION__: any;
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
+};
+
 export const getStore = () => {
-  return createStore(rootReducers, applyMiddleware(thunk));
+  /* eslint-disable no-underscore-dangle */
+  const composeEnhancers =
+    (window as MyWindow).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  return createStore(rootReducers, composeEnhancers(applyMiddleware(thunk)));
+  /* eslint-enable */
 };
