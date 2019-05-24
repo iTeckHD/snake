5a948655a04e6454be65fa40c17772e0efc65472
Tuan Kha Tran <tran.tuankha@muffag.ch>
2019-05-24 15:43:11 +0200

    fix(collision): fix collision
    ignore its own value

--- src/game/collision_check.ts
+++ src/game/collision_check.ts
@@ -3,11 +3,13 @@ import { GameConfig } from './config';
 
 export const collisionCheck = (coords: Coordination[]) => {
   return coords.some(
-    c1 =>
+    (c1, c1i) =>
       c1.x < 0 ||
       c1.y < 0 ||
       c1.x >= GameConfig.fieldSize ||
       c1.y >= GameConfig.fieldSize ||
-      coords.some(c2 => c1.x === c2.x && c1.y === c2.y),
+      coords
+        .filter((c2, c2i) => c1i !== c2i)
+        .some(c2 => c1.x === c2.x && c1.y === c2.y),
   );
 };
